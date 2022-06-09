b3307ae6313713451e15c8f93419709ad2143888
ybb <2362343323@qq.com>
2022-06-09 11:25:14 +0800

    second commit

--- src/main/java/com/rongyu/GitDemo.java
+++ src/main/java/com/rongyu/GitDemo.java
@@ -3,5 +3,6 @@ package com.rongyu;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("study git!");
+        System.out.println("Hello git!");
     }
 }
